d408e500ec91a9f4e7c50cf8f1d3fefa38f91bfd
jamesa820 <59709525+jamesa820@users.noreply.github.com>
2020-05-29 21:23:57 -0500

    Update app.js

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,81 +1,112 @@
-// Use d3.json() to fetch data from JSON file
-// Incoming data is internally referred to as importedData
-d3.json("./data/samples.json").then(function(importeddata){
-    console.log(importeddata.metadata);
 
-                    
-    //sorts id val
-    function desende(a,b) {
-        if(a.sample_values === undefined){
-            return b-a;
-        } else {
-
-            return b.sample_values[0] - a.sample_values[0];
-        }
+function optionChanged(sel) {
+        console.log(sel);
+        var personSelected = parseInt(sel);
+       
+        getData(personSelected);
     };
+function getData(personofInterest) {
+    d3.json("./data/samples.json").then(function (importeddata) {
+       
+        function desende(a, b) {
+            if (a.sample_values === undefined) {
+                return b - a;
+            } else {
+                return b.sample_values[0] - a.sample_values[0];
+            }
+        } 
+        console.log(personofInterest);
+        ray
+
+        var individual = importeddata.samples.filter(function (sample) {
+            return sample.id === String(personofInterest);
+        })[0];
+        
+        console.log(importeddata.metadata);
+        var individualMETA = importeddata.metadata.filter(function (sample) {
+        
+            return sample.id === parseInt((personofInterest));
+        })[0];
 
 
-    //convert string to integer
-    // var fatNames - importeddata.names.map(name => parseInt(name))
-    var fatNames = importeddata.names.map(name => parseInt(name));
-    var parsedNames = importeddata.names.map(function(name){
-        return parseInt(name)
+
+
+        var metaData = document.querySelector(".metaspot");
+        var metatoDisplay = JSON.stringify(individualMETA, null, 4);
+        // console.log(metaData);
+        metaData.innerText = metatoDisplay;
+
+        ///////////////////////////////////////////////////////////////////
+
+       renderGraphs(individual);
+    }); 
+};
+
+function buildSelector() {
+    d3.json("./data/samples.json").then(function (importeddata) {
+        var persontoget = document.querySelector("#selDataset");
+        var names = importeddata.names;
+        names.forEach(function (name) {
+            let option = document.createElement("option");
+            option.value = name;
+            option.innerText = name;
+            persontoget.append(option);
+        });
     });
+};
+function renderGraphs(individual){
+    function desende(a, b) {
+            if (a.sample_values === undefined) {
+                return b - a;
+            } else {
+                return b.sample_values[0] - a.sample_values[0];
+            }
+        } 
+     var qty = 10;
+        var trace1 = {
+            x: individual['sample_values'].slice(0, qty),
+            y: individual['otu_ids'].sort(desende).slice(0, qty),
+            text: individual['otu_labels'].slice(0, qty),
+            name: "BellyButton Stuff for:  ",
+            type: "bar",
+            orientation: "h"
+        };
+        var trace2 = {
+            x: individual['otu_ids'],
+            y: individual['sample_values'],
+            mode: 'markers',
+            marker: {
+                size: individual['sample_values']
+            },
+            text: individual['otu_labels'],
+            color: individual['otu_ids'],
+            name: "BellyButton Bubbles for:  "
+        };
+        var chartData1 = [trace1];
+        var chartData2 = [trace2];
+        var layout1 = {
+            title: "Belly Button Stuffings...",
+            margin: {
+                l: 50,
+                r: 50,
+                t: 50,
+                b: 50
+            }
+        };
+        var layout2 = {
+            title: "Belly Button Bubbles...",
+            margin: {
+                l: 50,
+                r: 50,
+                t: 50,
+                b: 50
+            }
+        };
+        // Render the plot to the div tag with id "plot"
+        Plotly.newPlot("plot1", chartData1, layout1);
+        Plotly.newPlot("plot2", chartData2, layout2);
 
-    //inividual to look at when query
-    var personofInterest = '1242';
-
-    //function to pull individual from samples
-    var individual = importeddata.samples.filter(function(sample){
-        return sample.id === personofInterest;
-    })[0];
-    
-    console.log(importeddata.metadata);
-    var individualMETA = importeddata.metadata.filter(function(sample){
-        console.log(sample.id);
-        return sample.id === parseInt((personofInterest));
-    })[0];
-
-    console.log("META Data");
-    console.log(individualMETA);
-
-    var qty = 10;
-    var trace1 = {
-        x: individual['sample_values'].slice(0,qty),
-        y: individual['otu_ids'].sort(desende).slice(0,qty),
-        text: individual['otu_labels'].slice(0,qty),
-        name: "BellyButton Stuff for:  ",
-        type: "bar",
-        orientation : "h"
-    };
-
-    var trace2 = {
-        x:individual['otu_ids'],
-        y: individual['sample_values'],
-        mode:'markers',
-        marker:{
-            size: individual['sample_values']
-        },
-        text: individual['otu_labels'],
-        color: individual['otu_ids'],
-        names: "Belly Button Bubbles for:  "
-                       
-    };
-
-    var chartData1 = [trace1];
-    var chartData2 = [trace2];
-    var layout = {
-        title: "Belly Button Stuffings...",
-        margin: {
-            l:50,
-            r:50,
-            t:50,
-            b:50
-        }
-    };
-
-    Plotly.newPlot("plot1", chartData1, layout);
-    Plotly.newPlot("plot2", chartData2, layout);
-
-});
+}
+buildSelector();
+getData(962);
 
